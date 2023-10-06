89651e9983426cef44b8d7d6f014418b01df4d27
Roho7 <rohosen2@gmail.com>
2023-10-06 19:43:09 +0100

    add:on scroll element

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,3 @@
-import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import LatestNews from "../components/LatestNews";
 import ArticleCarousel from "../components/ArticleCarousel";
@@ -6,11 +5,13 @@ import Catagories from "../components/Catagories";
 import Footer from "../components/Footer";
 import QuoteComponent from "../components/QuoteComponent";
 import StatsSection from "../components/StatsSection";
+import { motion, useScroll } from "framer-motion";
 
 function Homepage() {
+  const { scrollYProgress } = useScroll();
   return (
     <div>
-      <div>
+      <div className="relative z-[1]">
         <Hero />
         <br />
         <QuoteComponent />
@@ -25,6 +26,12 @@ function Homepage() {
         <br />
         <Footer />
       </div>
+      <motion.div
+        className="absolute top-[50vh] z-0 opacity-30"
+        style={{ scale: scrollYProgress }}
+      >
+        <img src="src/assets/bg-vector-1png.png" className="" />
+      </motion.div>
     </div>
   );
 }
