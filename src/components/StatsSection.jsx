fb53513f93ae5873496db81c15c48959d8583fa9
Roho7 <rohosen2@gmail.com>
2023-10-07 09:38:46 +0100

    mod: css fixes

--- src/components/StatsSection.jsx
+++ src/components/StatsSection.jsx
@@ -5,26 +5,26 @@ import { Book, PenTool, Shield, Users } from "react-feather";
 function StatsSection() {
   return (
     <motion.div
-      className="grid grid-cols-1 lg:grid-cols-4 gap-2 mx-2"
+      className="grid grid-cols-1 lg:grid-cols-4 mx-4 rounded-xl overflow-clip shadow-xl"
       initial={{ opacity: 0, y: 30 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 1, type: "spring" }}
     >
       <div className="stats-box bg-grass-100">
         <Users className="stats-icon" />
-        <h1>150</h1> Members
+        <h1 className="stats-header">7000+</h1> Members
       </div>
       <div className="stats-box bg-base-800 text-base-50">
         <Book className="stats-icon" />
-        <h1>1000+</h1> Blogs
+        <h1 className="stats-header">30+</h1> Blogs
       </div>
       <div className="stats-box bg-salmon-100">
         <Shield className="stats-icon" />
-        <h1>10 years</h1> of Commitment
+        <h1 className="stats-header">12+ years</h1> of Commitment
       </div>
       <div className="stats-box bg-base-50">
         <PenTool className="stats-icon" />
-        <h1>20</h1>
+        <h1 className="stats-header">20+</h1>
         Published Authors
       </div>
     </motion.div>
